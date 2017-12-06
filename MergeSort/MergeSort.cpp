1cf5ef88d23c5959a28f26929c03eecdc333e5ec
Vlada <vlada-kapranova@mail.ru>
2017-12-06 22:03:21 +0300

    Add QuickSort and MergeSort

--- MergeSort/MergeSort.cpp
+++ MergeSort/MergeSort.cpp
@@ -44,57 +44,90 @@ void Comp(int *arr, int left, int mid, int right)
 
 void MergeSort(int *arr, int left, int right)
 {
-	int mid;
-	if (left + 1 < right)
-	{
-		mid = (left + right) / 2;
-		MergeSort(arr, left, mid);
-		MergeSort(arr, mid, right);
-		Comp(arr, left, mid, right);
-	}
-}
-
-void test(int *arr_sort, int *arr_true, int len)
-{
-	int wrong_idx = 0;
-	bool correct_result = true;
-	MergeSort(arr_sort, 0, len);
-
-	for (int i = 0; i < len; i++)
-	{
-		if (arr_sort[i] != arr_true[i])
+		int mid;
+		if (left + 1 < right)
 		{
-			correct_result = false;
-			wrong_idx = i;
-			break;
+			mid = (left + right) / 2;
+			MergeSort(arr, left, mid);
+			MergeSort(arr, mid, right);
+			Comp(arr, left, mid, right);
 		}
-	}
-
-	if (correct_result == false)
-	{
-		printf("Error at idx %i, got %i, expected %i\n", wrong_idx, arr_sort[wrong_idx], arr_true[wrong_idx]);
-	}
-	else printf("Array sorted true\n");
 }
 
-int main(void)
+void print(int *arr, int len)
 {
-	int len = 10;
-	int arr[10] = {5,9,4,10,5,2,8,3,1,6};
-	int arr_sort[10] = {1,2,3,4,5,5,6,8,9,10};
-
-	/*for (int i = 0; i < len; i++)
-	{
-		arr[i] = rand()%10;
-	}*/
-
 	for (int i = 0; i < len; i++)
 	{
 		printf("%i_", arr[i]);
 	}
 	printf("\n");
+}
 
-	test(arr, arr_sort, len);
+void test(int *arr_sort, int *arr_true, int len)
+{
+	if (len != 0)
+	{
+		int wrong_idx = 0;
+		bool correct_result = true;
+		MergeSort(arr_sort, 0, len);
 
+		for (int i = 0; i < len; i++)
+		{
+			if (arr_sort[i] != arr_true[i])
+			{
+				correct_result = false;
+				wrong_idx = i;
+				break;
+			}
+		}
+
+		if (correct_result == false)
+		{
+			printf("Error at idx %i, got %i, expected %i\n", wrong_idx, arr_sort[wrong_idx], arr_true[wrong_idx]);
+		}
+		else printf("Array sorted true\n");
+	}
+	else printf("Array hasn't elements\n");
+}
+
+int main(void)
+{
+	int len1 = 10, len2 = 1, len3 = 2, len4 = 6, len5 = 5, len6 = 7;
+	int arr1[10] = {5,9,4,10,5,2,8,3,1,6};
+	int arr_sort1[10] = {1,2,3,4,5,5,6,8,9,10};
+	int arr2[1] = {5};
+	int arr_sort2[1] = {5};
+	int arr3[2] = { 5,3 };
+	int arr_sort3[2] = { 3,5 };
+	int arr4[6] = { 1,2,3,4,5,6 };
+	int arr_sort4[6] = { 1,2,3,4,5,6 };
+	int arr5[5] = { 10,9,8,7,6 };
+	int arr_sort5[5] = { 6,7,8,9,10 };
+	int arr6[7] = { 7,7,7,7,7,7,7 };
+	int arr_sort6[7] = { 7,7,7,7,7,7,7 };
+	
+	print(arr1, len1);
+	test(arr1, arr_sort1, len1);
+
+	print(arr2, len2);
+	test(arr2, arr_sort2, len2);
+
+	print(arr3, len3);
+	test(arr3, arr_sort3, len3);
+
+	print(arr4, len4);
+	test(arr4, arr_sort4, len4);
+
+	print(arr5, len5);
+	test(arr5, arr_sort5, len5);
+
+	print(arr6, len6);
+	test(arr6, arr_sort6, len6);
+
+	int *arr7 = (int*)malloc(0 * sizeof(int));
+
+	test(arr7, arr7, 0);
+
+	free(arr7);
 	return 0;
 }
